468d700bb71907e4350e9c593cd57d59c714201f
leonce2 <leoncefotso59@gmail.com>
2024-06-17 18:02:49 +0100

    quotes generator

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <AppHeader title="The anime Quote"/>
+    <AppQuote :quote="quote"/>
+    <div class="btn-container">
+      <button @click="getQuote">Generate</button>
+    </div>
+
+    <QuoteList :quotes="quotes"/>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppQuote from './components/AppQuote.vue'
+import QuoteList from './components/QuoteList.vue'
+
+
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    AppHeader,
+    AppQuote,
+    QuoteList
+  },
+
+  data(){
+    return{
+      quote: {
+        content: 'content goes here',
+        anime: 'Naruto',
+        character: 'Madara'
+      },
+      quotes:[]
+    }
+  },
+  methods: {
+    async getQuote(){
+      if(this.quote.content){
+        this.quotes = [...this.quotes, this.quote]
+      }
+
+      const data = await fetch('api/quotes').then(res => res.json());
+
+      this.quote = {
+        content: data.quote,
+        anime: data.anime,
+        character: data.character
+      }
+    }
+  },
+  created(){
+    this.getQuote();
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+*{
+  padding:0;
+  margin:0;
+  box-sizing:border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+:root{
+  --primary: #D81E5B;
+  --secondary: #8A4FFF;
+  --tertiary: #32CBFF;
+  --dark: #131A26;
+  --light: #EEE;
+  --grey: #848484;
+}
+
+.btn-container{
+  display: flex;
+  justify-content: center;
+  padding: 0px 32px;
+  margin: 64px auto;
+
+  button{
+    border:none;
+    outline: none;
+    background-color: var(--primary);
+    padding: 16px 32px;
+    border-radius: 99px;
+    color: var(--light);
+    font-size: 28px;
+    font-weight: 700;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: 0.4s;
+
+    &:hover{
+      background-color: var(--secondary);
+    }
+  }
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template lang="">
+   <header>
+    <h1>The anime Quote</h1>
+   </header>
+</template>
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+<style lang="scss" scoped>
+    header{
+        padding :24px 32px;
+        display: flex;
+        justify-content: center;
+
+        h1{
+            font-size: 32px;
+            color: var(--dark);
+            font-weight: 700;
+            text-transform: uppercase;
+            padding-bottom: 8px;
+            border-bottom: 5px solid #000;
+        }
+    }
+</style>
